6a165af4a00d42aa8cf64e8f93b524bfc1e15e23
Khoa Vu <khoa.vukhoa3004@hcmut.edu.vn>
2024-12-19 17:18:50 +0700

    Add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
