2934fe6a9bc558a60d827656df13dd7e10bc0d52
Khoa Vu <khoa.vukhoa3004@hcmut.edu.vn>
2024-12-19 21:05:13 +0700

    Fix the url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Image from "next/image";
+const preUrl = process.env.NEXT_PUBLIC_STATIC_URL || "";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={`${preUrl}/static-images/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -33,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={`${preUrl}/vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -59,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={`${preUrl}/file.svg`}
             alt="File icon"
             width={16}
             height={16}
@@ -74,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={`${preUrl}/window.svg`}
             alt="Window icon"
             width={16}
             height={16}
@@ -89,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={`${preUrl}/globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
