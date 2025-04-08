b144a09d315268ba6c44b9baeee79f836d950e92
Khoa Vu <khoa.vukhoa3004@hcmut.edu.vn>
2025-04-08 22:54:52 +0700

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly. hídfsdfas</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
